bb1b81c16ae60adffcd47b7ae66e9de21f7034a6
Emanuel Souza <emanuelsouza.1987@gmail.com>
2022-05-27 10:15:13 -0300

    Update script.js

--- hora_exata_01/script.js
+++ hora_exata_01/script.js
@@ -4,19 +4,20 @@ function carregar() {
     var data = new Date()  
     var hora = data.getHours()
     
+    
     msg.innerHTML = 'Agora são '+ hora + '  horas'  
     
     if(hora >= 0 && hora < 12){
         //Bom Dia!!
-        img.src = 'fotomaha.png'
+        img.src = 'manha.png'
         document.body.style.background = '#a6656b'
      }else if (hora >= 12 && hora < 18) {
            //Boa Tarde!!
-           img.src = 'fototarde.png'
+           img.src = 'tarde.png'
            document.body.style.background = '#f2b05'
      }else{
          //Boa Noite!!
-         img.src = 'fotonoite.png'
+         img.src = 'noite.png'
          document.body.style.background = '#2f2229'
      }
     }
